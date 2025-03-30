0ac46fa3f1ad908db096aca13f66b007a7f8eab9
Saul Ramirez <fernando.ramire.hn@gmail.com>
2025-03-29 19:04:05 -0600

    adding tests setup

---
+++ setup-tests.js
@@ -0,0 +1,33 @@
+const { Client } = require("pg");
+
+require("dotenv").config();
+
+const PORT = process.env.DATABASE_PORT || 5432;
+const USERNAME = process.env.DATABASE_USERNAME || "test";
+const PASSWORD = encodeURIComponent(process.env.DATABASE_PASSWORD || "test");
+const DATABASE = process.env.DATABASE || "testDB";
+const DATABASE_CLUSTER = process.env.DATABASE_CLUSTER || "localhost";
+
+const setupDatabase = async () => {
+    const DB_URL = `postgres://${USERNAME}:${PASSWORD}@${DATABASE_CLUSTER}:${PORT}/${DATABASE}`;
+
+    const dbClient = new Client(DB_URL);
+    console.log("Setting up the database...");
+
+    await dbClient.query(`CREATE DATABASE ${DATABASE};`);
+    await dbClient.query(`
+          CREATE TABLE if not exists users  (
+            id bigserial not null,
+            name varchar(100) not null,
+            password varchar(100) not null,
+            active bool not null default true,
+            created_at timestamp default now ());
+        `);
+    console.log("Database created successfully.");
+};
+
+const setUp = async () => {
+    await setupDatabase();
+};
+
+setUp();
